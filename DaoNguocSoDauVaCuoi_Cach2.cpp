ab5cd092a4411ea3ae9efd29e1e2cf2ff509700b
Quang lang thang <63640626+quanglangthang@users.noreply.github.com>
2022-11-12 09:13:45 +0700

    Update DaoNguocSoDauVaCuoi_Cach2.cpp

--- DaoNguocSoDauVaCuoi_Cach2.cpp
+++ DaoNguocSoDauVaCuoi_Cach2.cpp
@@ -3,23 +3,20 @@
 using namespace std;
 
 int main() {
-	int n, index = 0, dem = 0, dau;
-	double giua, daonguoc = 0, cuoi;
+	int n,index,dau,than,cuoi,dem = 0, dao;
 	cin >> n;
 	index = n;
-	while (n > 0) {
-		n /= 10;
+	while (index > 0) {
+		index /= 10;
 		dem++;
 	}
-	dau = index / pow(10,dem-1);
-	cuoi = index % 10; 
-	giua = (index / 10) * pow(10,(-1)*(dem-1));
-// 	Lay so dau x 10^-1 de lay so giua - dau de ra dong 21
-	daonguoc = dau * pow(10,-1);
-
-//	De tach than so dang 12345 => 0.0234
-	double temp = giua - daonguoc;
-	daonguoc = (cuoi * pow(10,dem-2) + temp*pow(10,dem-1))*10 + dau;
-	cout << daonguoc;
+//	cout << dem;
+	cuoi = n % 10;
+	dau = n / pow(10,dem-1);
+//	cout << dau << " " << cuoi;
+	than = n - (dau*pow(10,dem-1)+cuoi);
+//	cout << than;
+	dao = cuoi * pow(10,dem-1) + than + dau;
+	cout << dao;
 	
 }
