c905df367b56dcc76bd96d9f23eba77d63e25527
Quang lang thang <63640626+quanglangthang@users.noreply.github.com>
2022-11-16 16:58:53 +0700

    Update BangCuuChuong.cpp

--- BangCuuChuong.cpp
+++ BangCuuChuong.cpp
@@ -5,48 +5,7 @@ int main() {
 	int n;
 	cout << "Nhap bang cuu chuong: ";
 	cin >> n;
-	switch (n) {
-		case 2:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 3:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 4:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 5:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 6:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 7:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 8:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		case 9:
-			for (int i = 1; i <= 10; i++) {
-				cout << n << " x " << i << " \t= " << n*i << endl;
-			}
-			break;
-		default: 
-			cout << "Loi";	
+	for (int i = 1; i <= 10; i++) {
+		cout << n << "\tx " << i << "\t=" << n*i;
 	}
 }
